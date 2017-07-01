36b2f66474d70f1ce8249db492f4c8abcd18e6d4
Douwe Homans <douwe@avocado.nl>
2017-07-01 16:30:07 +0200

    Procfile setup for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 config :trechter, Trechter.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  cache_static_manifest: "priv/static/manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "trechter.herokuapp.com", port: 443],
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :trechter, Trechter.Repo,
   adapter: Ecto.Adapters.Postgres,
